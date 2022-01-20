5979386e17b3b81a20fe7917ef322cabba251f14
tezosmiami <tezosmiami@gmail.com>
2022-01-19 20:45:55 -0500

    server

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+require('dotenv').config();
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId } = require('./config.json');
+const { clientId, guildId} = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const fs = require('fs');
-const { Client, Collection, Intents } = require('discord.js');
+// const keepAlive = require("./server")
 
+const { Client, Collection, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data?.name, command);
-}
-
-client.on('ready', () => {
+client.once('ready', () => {
 	console.log('Ready!');
 });
+
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
@@ -30,5 +24,5 @@ client.on('interactionCreate', async interaction => {
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
-
+// keepAlive()
 client.login(process.env.DISCORD_TOKEN);
