1c15c4bfee631b9c12708f03bccf70828f3e6903
tezosmiami <tezosmiami@gmail.com>
2022-01-23 22:37:01 -0500

    Update marco.js

--- commands/marco.js
+++ commands/marco.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('marco')
-		.setDescription('like the game...'),
+		.setDescription('like the game. . .'),
 	async execute(interaction) {
 		await interaction.reply('polo!');
 	},
