8c28e4a0a44ee638b6dce38a1b8417459aa19173
tezosmiami <tezosmiami@gmail.com>
2022-01-23 22:59:27 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('classic...'),
+		.setDescription('classic. . .'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
 	},
